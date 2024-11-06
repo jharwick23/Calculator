f382478005a59b0bee0f4d45a55d0c40edc42b9e
jharwick23 <146146809+jharwick23@users.noreply.github.com>
2024-11-05 22:00:54 -0500

    Create operator function along with basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {return a + b;}
+function subtract(a, b) {return a - b;}
+function multiply(a, b) {return a * b;}
+function division(a, b) {return a / b;}
+function remainder(a, b) {return a % b;}
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+': return add(a,b);
+        case '-': return subtract(a,b);
+        case '*': return multiply(a,b);
+        case '/': return b === 0 ? "Error" : division(a,b);
+        case '%': return b === 0 ? "Error" : remainder(a,b);
+    }
+}
